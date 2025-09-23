9b0f532a7984447644d94f465f22574d1e5bd911
JvCorMo <jvcormo@gmail.com>
2025-09-24 00:38:00 +0200

    Preparando y comprendiendo el fincionamiento de onClick en AS

--- app/src/main/java/com/example/juegoclicker/MainActivity.kt
+++ app/src/main/java/com/example/juegoclicker/MainActivity.kt
@@ -1,48 +1,38 @@
 package com.example.juegoclicker
 
 import android.os.Bundle
-import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.expandHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.juegoclicker.ui.theme.JuegoClickerTheme
-
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -69,7 +59,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     val img1 = painterResource(R.drawable.principal);
-    var puntos: Int = 0;
+    var punto  by remember { mutableStateOf(0) }
     Surface(
         color = MaterialTheme.colorScheme.background,
     ) {
@@ -121,7 +111,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     horizontalArrangement = Arrangement.Center,
                 ){
                     Button(
-                        onClick = {puntos++},
+                        onClick = { punto++ },
                         modifier = Modifier.padding(top = 600.dp),
                     ) {
                         Text(
@@ -134,7 +124,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 } // END Row
                 Text(
                     color = colorResource(R.color.miColor),
-                    text = "Los puntos que estas obteniendo son: $puntos", // RECUERDA $puntos- Tengo que poner variable del contador
+                    text = "Los puntos que estas obteniendo son: $punto", // RECUERDA $puntos- Tengo que poner variable del contador
                     fontSize = 18.sp,
                     modifier = Modifier.align(Alignment.Start).padding(top = 18.dp, start = 15.dp),
                 )
